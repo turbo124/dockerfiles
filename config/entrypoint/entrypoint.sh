66b89e887f59b9e60275cfa0d9b3ccbc80e1821a
David Bomba <turbo124@gmail.com>
2024-06-03 16:07:53 +1000

    Debian build

--- config/entrypoint/entrypoint.sh
+++ config/entrypoint/entrypoint.sh
@@ -5,6 +5,18 @@ in_error() {
     exit 1
 }
 
+in_log() {
+    local type="$1"
+    shift
+    printf '%s [%s] [Entrypoint]: %s\n' "$(date -u '+%Y-%m-%dT%H:%M:%SZ')" "$type" "$*"
+}
+
+chown -R www-data:www-data /var/www
+chmod 775 /var/www/storage/logs
+chmod 775 /var/www/storage/framework
+chmod 775 /var/www/storage/cache
+chmod 775 /var/www/bootstrap/cache
+
 php artisan optimize
 
 # Check if DB works, if not crash the app.
@@ -15,6 +27,8 @@ if [ "$DB_READY" != "1" ]; then
 fi
 
 php artisan migrate --force
+
+php artisan storage:link
 
 vendor/bin/snappdf download
 
